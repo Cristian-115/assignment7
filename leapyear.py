7fa1f731b5db285050d8dda2c0fda0d90084a890
Cristian Garibay <garibcri@oregonstate.edu>
2021-03-07 22:12:03 -0800

    second test works

--- leapyear.py
+++ leapyear.py
@@ -4,5 +4,9 @@ def isleap(year):
         array.append("year was not positive")
     if year % 4 == 0 and year % 100 != 0:
         array.append(str(year) + " is a leap year")
-   
+    elif year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+        array.append(str(year) + " is a leap year!")
+
+    else:
+        array.append(str(year) + " not a leap year")
     return array
