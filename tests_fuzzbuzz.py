ed021af17c3e74caddb7bb8642ce3d41d5cd0231
Cristian Garibay <garibcri@oregonstate.edu>
2021-03-07 21:51:53 -0800

    wrote second test

--- tests_fuzzbuzz.py
+++ tests_fuzzbuzz.py
@@ -8,7 +8,11 @@ class TestCase(unittest.TestCase):
         self.assertEqual(res[0], "Buzz")
         
 
-
-
+    def test_values(self):
+        res = fuzzbuzz.fuzzbuzz(5)
+        self.assertEqual(res[1], 1)
+        self.assertEqual(res[2], 2)
+        self.assertEqual(res[3], 3)
+        self.assertEqual(res[4], 4)
 if __name__ == '__main__':
-     unittest.main()
+    unittest.main()
