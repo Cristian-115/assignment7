80de36cd576480157468d6cb3ed4b3fcb0fc24c7
Cristian Garibay <garibcri@oregonstate.edu>
2021-03-07 21:53:32 -0800

    made sure test 2 works

--- fuzzbuzz.py
+++ fuzzbuzz.py
@@ -4,7 +4,7 @@ def fuzzbuzz(value):
     for i in range(value):
         if i % 5 == 0:
             array.append("Buzz")
-       
+        
         else:
             array.append(i)
     array.pop(0)
