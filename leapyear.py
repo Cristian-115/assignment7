616f94ecd6df6c56cb77bd7af8768435f4455c34
Cristian Garibay <garibcri@oregonstate.edu>
2021-03-07 22:11:14 -0800

    wrote second test

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,8 @@
 def isleap(year):
     array = []
+    if year < 0:
+        array.append("year was not positive")
     if year % 4 == 0 and year % 100 != 0:
         array.append(str(year) + " is a leap year")
-
+   
     return array
--- tests_leapyear.py
+++ tests_leapyear.py
@@ -3,9 +3,13 @@ import leapyear
 
 class TestCase(unittest.TestCase):
     
-    def test_Fizz(self):
+    def test_leap(self):
         res = leapyear.isleap(2012)
         self.assertEqual(res[0], "2012 is a leap year")
+
+    def test_notleap(self):
+        res = leapyear.isleap(2020)
+        self.assertEqual(res[0], "2020 not a leap year")
         
 
 if __name__ == '__main__':
