5e7bd563bcb58bd0f840ab230373887cb2bada99
Cristian Garibay <garibcri@oregonstate.edu>
2021-03-07 21:53:53 -0800

    finished the function

--- fuzzbuzz.py
+++ fuzzbuzz.py
@@ -4,7 +4,10 @@ def fuzzbuzz(value):
     for i in range(value):
         if i % 5 == 0:
             array.append("Buzz")
-        
+        elif i % 3 == 0:
+            array.append("Fizz")
+        elif i % 5 == 0 and i % 3 == 0:
+             array.append("FizzBuzz")
         else:
             array.append(i)
     array.pop(0)
